a3ce982403e8e130df8f039c6670c0277e84f496
Einas <et86et@hotmail.com>
2022-10-27 10:01:14 +0300

    Initial Commit

---
+++ GIT Test/GIT_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_TestApp.swift
+//  GIT Test
+//
+//  Created by Einas Alturki on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GIT_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
