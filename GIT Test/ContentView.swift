2dbc7fd0c736e9a2bc4d3bf0a6ee8ecda44a45a5
Einas <et86et@hotmail.com>
2022-10-27 12:01:46 +0300

    removed the background image

--- GIT Test/ContentView.swift
+++ GIT Test/ContentView.swift
@@ -9,16 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+//        ZStack{
+//
+//            Image("back")
+//                .resizable()
+//                .frame(width:800,height:800)
+//             //   .cornerRadius(100)
+//
+            VStack {
+       
+                ZStack{
+                    
+                    Circle()
+                        .stroke(.yellow,lineWidth:2)
+                        .frame(width: 150,height: 150)
+                    
+                    Image("butterfly")
+                        .resizable()
+                        .frame(width:150,height:150)
+                        .cornerRadius(100)
+                    
+                }
+                Text("Einas Alturki").font(.largeTitle)
+                
+                VStack(alignment: .leading)
+                {
+                    
+                    
+                    
+                    Text("I'm a mom of 3 beautiful girls. love to travel and do new things.Thinker outside of the box. Excited to be in the academy hope time will be kind with me and may the odds be ever in my favor.")
+                        .padding()
+                        .border(.yellow)
+                    
+                }
+            }
+            .padding()
         }
-        .padding()
-    }
+   // }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
